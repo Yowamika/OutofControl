60faa36c6bed2a3fd860af00dd0c1f05fe70257c
seikuu2 <reikuu2@yahoo.co.jp>
2021-01-20 10:33:10 +0900

    ジェネレータースタンプ(途中

--- Assets/Script/StampGenerator/Generator.cs
+++ Assets/Script/StampGenerator/Generator.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using System.IO;
+using System.Linq;
+using UnityEngine.UI;
 
 public class Generator : MonoBehaviour
 {
     // ステージ生成用スクリプト
+    [SerializeField]
     ObjectGenerator objectGenerator;
 
-
+    string csvDataFile = "/Resources/Data/csv/Stamp/";
+    private void Awake()
+    {
+        
+    }
     /// <summary>
     /// スタンプファイルを基にプレファブを生成する関数
     /// </summary>
